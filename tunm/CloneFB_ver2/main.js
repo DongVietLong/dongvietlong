fa6236a4a95b1eb2a9edb28e4787df2db539e839
tunm98 <nguyenmanhtu.ftu@gmail.com>
2022-07-24 16:55:00 +0700

    fb homework

--- tunm/CloneFB_ver2/main.js
+++ tunm/CloneFB_ver2/main.js
@@ -1,3 +1,20 @@
+var x = document.getElementById("toggle");
+var modal = document.getElementById("myModal");
+
+window.onclick = function (event) {
+  console.log(event.target);
+  if (event.target == modal) {
+    x.style.display = "none";
+    modal.style.display = "none";
+  }
+};
+
 function toggle() {
-    document.getElementById("more").style.display="flex"
+  if ((x.style.display = "none")) {
+    x.style.display = "flex";
+    modal.style.display = "block";
+  } else {
+    x.style.display = "none";
+    modal.style.display = "none";
+  }
 }
