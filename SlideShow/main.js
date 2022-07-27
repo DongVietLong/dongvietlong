4922d1233a466c7ae01c522f7ae7fc42377589f9
tunm98 <nguyenmanhtu.ftu@gmail.com>
2022-07-28 04:10:11 +0700

    slideShow

--- SlideShow/main.js
+++ SlideShow/main.js
@@ -1,5 +1,8 @@
 let slideIndex = 0;
 let getSlide = document.getElementsByClassName("slides");
+let getDots = document.getElementsByClassName("dot");
+// let timer = 2000;
+
 getSlide[0].style.display = "flex";
 
 function nextSlide() {
@@ -8,6 +11,10 @@ function nextSlide() {
     slideIndex = 0;
   }
   showSlide(slideIndex);
+  timer = null;
+//   setTimeout(function () {
+//     timer = 1000;
+//   }, 5000);
 }
 function prevSlide() {
   slideIndex -= 1;
@@ -21,8 +28,13 @@ function showSlide(index) {
   for (let i = 0; i < getSlide.length; i++) {
     if (i == index) {
       getSlide[i].style.display = "flex";
+      getDots[i].style.backgroundColor = "orange";
     } else {
       getSlide[i].style.display = "none";
+      getDots[i].style.backgroundColor = "white";
     }
   }
 }
+// setInterval(function () {
+//   nextSlide();
+// }, timer);
