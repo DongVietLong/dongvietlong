3b77615e4ee7f3b6bf07adaef6da6910df7d0023
truongdq.dev <truongdq.dev@gmail.com>
2022-08-08 20:46:32 +0700

    update sample react js

---
+++ gvtruong/react-vite-app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AppLogo from "./components/AppLogo";
+import ButtonIncrement from "./components/ButtonIncrement";
+
+function App() {
+  return (
+    <div className="App">
+      <AppLogo />
+      <ButtonIncrement />
+    </div>
+  );
+}
+
+export default App;
